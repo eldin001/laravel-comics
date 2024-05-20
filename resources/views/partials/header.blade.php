57d1c42f102278d469d0a3dae64fcf353bf92b5d
eldin001 <eldin.balje@icloud.com>
2024-05-20 15:19:17 +0200

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,10 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header class="bg-white">
+        <div class="container d-flex justify-content-between mt-3 mb-3  ">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="Logo DC comics">
+            <ul class="d-flex align-items-center list-unstyled gap-3">
+                <li v-for="(item, index) in menu" :key="index">
+                <a class="text-decoration-none text-uppercase" :href="item.url">{{ item.title }}</a>
+                </li>
+            </ul>
+        </div>
+    </header>
