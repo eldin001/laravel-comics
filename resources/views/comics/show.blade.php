c0ea2ef2d6fdd9af1e5ddc03aad8636e66232153
eldin001 <eldin.balje@icloud.com>
2024-05-29 17:56:22 +0200

    added style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,7 +7,6 @@
     <h1>{{ $comic->title }}</h1>
     <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
     <p>{{ $comic->description }}</p>
-    <!-- Altri dettagli del comic -->
     <a href="{{ route('comics.index') }}">Back to All Comics</a>
 </main>
 @endsection
